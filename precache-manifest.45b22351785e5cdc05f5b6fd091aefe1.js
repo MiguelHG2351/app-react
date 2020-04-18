a8c31e1362bb3d16630803b8ca26998b08736881
Miguel Hernández Gaitan <60478285+MiguelHG2351@users.noreply.github.com>
2020-04-18 01:35:33 -0600

    Updates

---
+++ precache-manifest.45b22351785e5cdc05f5b6fd091aefe1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93a450c9684efa1b5103492a4d3a6998",
+    "url": "/app-react/index.html"
+  },
+  {
+    "revision": "660633136c63ca03b701",
+    "url": "/app-react/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c894e8a8e01d6a599ce8",
+    "url": "/app-react/static/js/2.07a05dc4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/app-react/static/js/2.07a05dc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "660633136c63ca03b701",
+    "url": "/app-react/static/js/main.45022b81.chunk.js"
+  },
+  {
+    "revision": "1ca4d4b407e5c355ff87",
+    "url": "/app-react/static/js/runtime-main.b82a6044.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/app-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
