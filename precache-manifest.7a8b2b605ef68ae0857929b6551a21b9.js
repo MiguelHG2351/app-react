3b017654ef62425cf4e737374c4d70d26120067c
Miguel Hernández Gaitan <60478285+MiguelHG2351@users.noreply.github.com>
2020-04-27 06:11:00 -0600

    Updates

---
+++ precache-manifest.7a8b2b605ef68ae0857929b6551a21b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24779ae7123714d01d45453190a7dad5",
+    "url": "/app-react/index.html"
+  },
+  {
+    "revision": "602d606df52776b43730",
+    "url": "/app-react/static/css/main.457e36e5.chunk.css"
+  },
+  {
+    "revision": "825b6441b2d1d3962623",
+    "url": "/app-react/static/js/2.31735d17.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/app-react/static/js/2.31735d17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "602d606df52776b43730",
+    "url": "/app-react/static/js/main.297681ae.chunk.js"
+  },
+  {
+    "revision": "1ca4d4b407e5c355ff87",
+    "url": "/app-react/static/js/runtime-main.b82a6044.js"
+  },
+  {
+    "revision": "de11d7783bef61bf43bdfa1a1adc3c70",
+    "url": "/app-react/static/media/task.de11d778.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/app-react/precache-manifest.88ec27c5f640cdd775650551cba9c377.js"
+  "/app-react/precache-manifest.7a8b2b605ef68ae0857929b6551a21b9.js"
 );
 
 self.addEventListener('message', (event) => {
